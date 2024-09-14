76bc4caf5ae18d0e809a24190259b86b4d2832dd
Orion Pobursky <orion@ldraw.org>
2024-09-14 20:57:39 +0000

    Remove backup of static assests now part of repo

--- app/Console/Commands/Backup.php
+++ app/Console/Commands/Backup.php
@@ -51,26 +51,9 @@ class Backup extends Command
             $zip->addFile(base_path('.env'), '.env');
             $zip->addFile($db_backup, 'storage/app/backup/db_backup.sql');
 
-            foreach (Storage::disk('local')->allFiles('assets') as $file) {
-                $zip->addFile(Storage::disk('local')->path($file), "storage/app/{$file}");
-            }
-
             foreach (Storage::disk('local')->allFiles('deleted') as $file) {
                 $zip->addFile(Storage::disk('local')->path($file), "storage/app/{$file}");
             }
-
-            foreach (Storage::disk('images')->allFiles('banners') as $file) {
-                $zip->addFile(Storage::disk('images')->path($file), "storage/app/images/{$file}");
-            }
-            foreach (Storage::disk('images')->allFiles('cards') as $file) {
-                $zip->addFile(Storage::disk('images')->path($file), "storage/app/images/{$file}");
-            }
-            foreach (Storage::disk('images')->allFiles('updates') as $file) {
-                $zip->addFile(Storage::disk('images')->path($file), "storage/app/images/{$file}");
-            }
-            $zip->addFile(Storage::disk('images')->path('LDraw_Black_64x64.png'), 'storage/app/images/LDraw_Black_64x64.png');
-            $zip->addFile(Storage::disk('images')->path('LDraw_Green_64x64.png'), 'storage/app/images/LDraw_Green_64x64.png');
-            $zip->addFile(Storage::disk('images')->path('LDraw_Orange_64x64.png'), 'storage/app/images/LDraw_Orange_64x64.png');
 
             foreach (Storage::disk('library')->allFiles('official') as $file) {
                 $zip->addFile(Storage::disk('library')->path($file), "storage/app/library/{$file}");
