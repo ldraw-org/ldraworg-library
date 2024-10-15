ca95ecd39767f815212c3f9925fe04bdde9df003
Orion Pobursky <orion@ldraw.org>
2024-10-15 02:50:06 +0000

    Add minor edits table

---
+++ app/Livewire/Tables/OfficialMinorEdits.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Livewire\Tables;
+
+use App\Models\Part;
+use App\Filament\Part\Tables\PartTable;
+use Filament\Tables\Table;
+
+class OfficialMinorEdits extends BasicTable
+{
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(
+                Part::official()
+                    ->where('has_minor_edit', true)
+                    ->orderBy('part_type_id')
+                    ->orderBy('filename')
+            )
+            ->emptyState(view('tables.empty', ['none' => 'None']))
+            ->columns(PartTable::columns())
+            ->actions(PartTable::actions())
+            ->recordUrl(fn (Part $p): string => route('tracker.show', ['part' => $p]));
+    }
+
+}
--- resources/views/part/nextrelease.blade.php
+++ resources/views/part/nextrelease.blade.php
@@ -10,6 +10,16 @@
         These are the parts that currently qualify for the next update. While the
         parts on this list will generally be released in the next update, some of them 
         may be manually held back by the the Library Admin for various other reasons.
-    </p>  
-    <livewire:tables.next-release-parts-table />
+    </p>
+    <p>  
+        <livewire:tables.next-release-parts-table />
+    </p>
+    <p>
+        <x-accordion id="officialMinorEdits">
+            <x-slot name="header" class="text-md font-bold">
+                Official Parts with Minor Edits (Changes to keywords, license, and/or user/real name)
+            </x-slot>
+            <livewire:tables.official-minor-edits />
+        </x-accordion>
+    </p>
 </x-layout.tracker>
