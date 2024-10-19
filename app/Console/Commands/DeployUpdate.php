d1a9661d5a5b716afe1f1ff4d34292d507c6f840
Orion Pobursky <orion@ldraw.org>
2024-10-19 03:25:53 +0000

    Remove code code

--- app/Console/Commands/DeployUpdate.php
+++ app/Console/Commands/DeployUpdate.php
@@ -3,7 +3,6 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Spatie\Permission\Models\Permission;
 
 class DeployUpdate extends Command
 {
@@ -26,10 +25,5 @@ class DeployUpdate extends Command
      */
     public function handle(): void
     {
-        // Reset cached roles and permissions
-        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
-
-        // create permissions
-        Permission::create(['name' => 'part.keyword.edit']);
     }
 }
