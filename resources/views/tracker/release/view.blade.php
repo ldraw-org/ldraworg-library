57cdfebd6634c22f85ed2714de36ebf40ccc6865
Orion Pobursky <orion@ldraw.org>
2024-11-08 14:37:04 +0000

    Allow copy past again

--- resources/views/tracker/release/view.blade.php
+++ resources/views/tracker/release/view.blade.php
@@ -4,14 +4,17 @@
     <div class="flex flex-col space-y-2">
         @isset($release->part_list) 
             @foreach($release->part_list as list($description, $filename))
-                <a href="{{route('parts.show', App\Models\Part::official()->where('filename', $filename)->first())}}">
                     <div class="rounded border w-fit">
-                        <div class="font-bold bg-gray-200 p-2">
-                            {{$filename}} - {{$description}}
+                        <div class="flex flex-row space-x-2 items-center font-bold bg-gray-200 p-2">
+                            <div>
+                                {{$filename}} - {{$description}}
+                            </div>
+                            <a class="rounded p-2 my-1 border border-gray-950 bg-gray-100" href="{{route('parts.show', App\Models\Part::official()->where('filename', $filename)->first())}}">
+                                View Part
+                            </a>
                         </div>
                         <img class="p-2" src="{{asset('images/library/updates/view' . $release->short . '/' . substr($filename, 0, -4) . '.png')}}">
                     </div>
-                </a>
             @endforeach  
         @else
             <p>
