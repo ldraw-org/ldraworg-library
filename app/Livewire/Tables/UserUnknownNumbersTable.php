1d97862da31706ecda19b5d23cb41bc89a8de5bb
Orion Pobursky <orion@ldraw.org>
2024-11-16 21:37:40 +0000

    Add description

--- app/Livewire/Tables/UserUnknownNumbersTable.php
+++ app/Livewire/Tables/UserUnknownNumbersTable.php
@@ -11,6 +11,7 @@ use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\TextInputColumn;
 use Filament\Tables\Table;
 use LaraZeus\Quantity\Components\Quantity;
+use Livewire\Attributes\Computed;
 
 class UserUnknownNumbersTable extends BasicTable
 {
@@ -26,7 +27,15 @@ class UserUnknownNumbersTable extends BasicTable
                     ->state(fn (UnknownPartNumber $unk) => "u{$unk->number}")
                     ->sortable(),
                 TextColumn::make('in_use')
-                    ->state(fn(UnknownPartNumber $unk) => is_null(Part::doesntHave('official_part')->firstWhere('filename', 'LIKE', "parts/u{$unk->number}%.dat")) ? 'No' : 'Yes'),
+                    ->state(fn(UnknownPartNumber $unk) => is_null($this->findUPart($unk)) ? 'No' : 'Yes'),
+                TextColumn::make('in_use_desciption')
+                    ->state(function (UnknownPartNumber $unk) {
+                        $p = $this->findUPart($unk);
+                        if (!is_null($p)) {
+                            return $p->description;
+                        }
+                        return '';
+                    }),
                 TextInputColumn::make('notes')
                     ->rules(['required', 'max:255'])
             ])
@@ -34,11 +43,11 @@ class UserUnknownNumbersTable extends BasicTable
             ->actions([
                 Action::make('view')
                     ->url(fn(UnknownPartNumber $unk) => route('parts.list', ['tableSearch' => "u{$unk->number}"]))
-                    ->visible(fn(UnknownPartNumber $unk) => !is_null(Part::doesntHave('official_part')->firstWhere('filename', 'LIKE', "parts/u{$unk->number}%.dat"))),
+                    ->visible(fn(UnknownPartNumber $unk) => !is_null($this->findUPart($unk))),
                 DeleteAction::make('delete')
                     ->modalHeading(fn(UnknownPartNumber $unk) => "Remove your reservation of u{$unk->number}?")
                     ->modalDescription('Are you sure you\'d like to remove this number from your reservation list? This cannot be undone.')
-                    ->visible(fn(UnknownPartNumber $unk) => is_null(Part::doesntHave('official_part')->firstWhere('filename', 'LIKE', "parts/u{$unk->number}%.dat")))
+                    ->visible(fn(UnknownPartNumber $unk) => is_null($this->findUPart($unk)))
             ])
             ->headerActions([
                 Action::make('request')
@@ -75,5 +84,17 @@ class UserUnknownNumbersTable extends BasicTable
                     })
             ])
             ->paginated(false);
-   }
+    }
+
+    #[Computed]
+    protected function uParts()
+    {
+        return Part::doesntHave('official_part')->where('filename', 'LIKE', 'parts/u____%.dat')->get();
+    }
+
+    protected function findUPart(UnknownPartNumber $unk): ?Part 
+    {
+        $p = $this->uParts->first(fn (Part $part) => strpos($part->filename, "parts/u{$unk->number}") !== false);
+        return $p;
+    }
 }
