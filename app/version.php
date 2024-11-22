1fd40056c4abee4998c09d4891e6889f1c38aad6
Orion Pobursky <orion@ldraw.org>
2024-11-22 03:26:50 +0000

    ensure no conflict

--- app/version.php
+++ app/version.php
@@ -1,11 +1,13 @@
 <?php
 
-function version($path): string
-{
-    $filePath = public_path($path);
-    if (!file_exists($filePath)) {
-        return asset($path);
-    }
+if (!function_exists('version')) {
+    function version(string $path): string
+    {
+        $filePath = public_path($path);
+        if (!file_exists($filePath)) {
+            return asset($path);
+        }
 
-    return asset($path) . "?v=" . filemtime($filePath);
+        return asset($path) . "?v=" . filemtime($filePath);
+    }
 }
