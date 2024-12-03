7bbbbd4a18ee41324ade5a5070252b83612e9146
Orion Pobursky <orion@ldraw.org>
2024-12-03 16:09:56 +0000

    Preferrentially show unofficial parts.

--- resources/views/livewire/search/suffix.blade.php
+++ resources/views/livewire/search/suffix.blade.php
@@ -3,7 +3,7 @@
 </x-slot>
 <x-slot:breadcrumbs>
   <x-breadcrumb-item class="active" item="Pattern Search" />
-</x-slot>    
+</x-slot>
 <div>
     <form class="p-2" wire:submit="doSearch">
         {{ $this->form }}
@@ -21,12 +21,12 @@
             Pattern/Composite/Sticker Shortcut Reference for {{$part->name()}} - {{$part->description}}
         </div>
         <x-filament::tabs class="p-2">
-            <x-filament::tabs.item 
+            <x-filament::tabs.item
                 :active="$activeTab === 'patterns'"
                 wire:click="$set('activeTab', 'patterns')"
             >
                 <x-slot name="badge">
-                    {{$part->patterns->count()}}
+                    {{$part->patterns->whereNull('unofficial_part')->count()}}
                 </x-slot>
                 Patterns
             </x-filament::tabs.item>
@@ -35,33 +35,33 @@
                 wire:click="$set('activeTab', 'composites')"
             >
                 <x-slot name="badge">
-                    {{$part->composites->count()}}
+                    {{$part->composites->whereNull('unofficial_part')->count()}}
                 </x-slot>
                 Composites
             </x-filament::tabs.item>
-        
+
             <x-filament::tabs.item
                 :active="$activeTab === 'shortcuts'"
                 wire:click="$set('activeTab', 'shortcuts')"
             >
                 <x-slot name="badge">
-                    {{$part->shortcuts->count()}}
+                    {{$part->shortcuts->whereNull('unofficial_part')->count()}}
                 </x-slot>
                 Shortcuts
             </x-filament::tabs.item>
 
         </x-filament::tabs>
-        
+
         <div class="rounded border p-2">
             @switch($activeTab)
                 @case('patterns')
-                    <x-part.grid :parts="$part->patterns->load('votes', 'official_part')->sortBy('filename')" />
+                    <x-part.grid :parts="$part->patterns->load('votes', 'official_part')->whereNull('unofficial_part')->sortBy('filename')" />
                     @break
                 @case('composites')
-                    <x-part.grid :parts="$part->composites->load('votes', 'official_part')->sortBy('filename')" />
+                    <x-part.grid :parts="$part->composites->load('votes', 'official_part')->whereNull('unofficial_part')->sortBy('filename')" />
                     @break
                 @case('shortcuts')
-                    <x-part.grid :parts="$part->shortcuts->load('votes', 'official_part')->sortBy('filename')" />
+                    <x-part.grid :parts="$part->shortcuts->load('votes', 'official_part')->whereNull('unofficial_part')->sortBy('filename')" />
                     @break
             @endswitch
         </div>
