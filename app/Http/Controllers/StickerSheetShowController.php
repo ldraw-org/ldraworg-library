a4797b7f9adf7cfb8f1803115873ee7d711756dc
Orion Pobursky <orion@ldraw.org>
2024-12-10 17:36:19 +0000

    Reorder lists

--- app/Http/Controllers/StickerSheetShowController.php
+++ app/Http/Controllers/StickerSheetShowController.php
@@ -13,12 +13,19 @@ class StickerSheetShowController extends Controller
     public function __invoke(Request $request, StickerSheet $sheet)
     {
         $flat = $sheet->parts
+            ->whereNull('unofficial_part')
             ->where('category.category', 'Sticker')
-            ->filter(fn(Part $part, int $key) => !Str::contains($part->description, '(Formed)'));
+            ->filter(fn(Part $part, int $key) => !Str::contains($part->description, '(Formed)'))
+            ->sortBy('filename');
         $formed = $sheet->parts
+            ->whereNull('unofficial_part')
             ->where('category.category', 'Sticker')
-            ->filter(fn(Part $part, int $key) => Str::contains($part->description, '(Formed)'));
-        $shortcuts = $sheet->parts->where('category.category', '<>', 'Sticker');
+            ->filter(fn(Part $part, int $key) => Str::contains($part->description, '(Formed)'))
+            ->sortBy('filename');
+        $shortcuts = $sheet->parts
+            ->whereNull('unofficial_part')
+            ->where('category.category', '<>', 'Sticker')
+            ->sortBy('filename');
         return view('sticker-sheet.show', compact('sheet', 'flat', 'formed', 'shortcuts'));
     }
 }
