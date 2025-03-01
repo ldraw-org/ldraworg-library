fb8ab6e7948c4f341b620538b8f2a85bf3b731c0
Orion Pobursky <orion@ldraw.org>
2025-03-01 21:04:56 +0000

    Fix countable error

--- app/LDraw/Check/Checks/HistoryIsValid.php
+++ app/LDraw/Check/Checks/HistoryIsValid.php
@@ -13,11 +13,11 @@ class HistoryIsValid implements Check
     public function check(ParsedPart|Part $part, Closure $fail): void
     {
         if ($part instanceof ParsedPart) {
-            $hcount = count($part->history);
+            $hcount = count($part->history ?? []);
             if ($hcount != mb_substr_count($part->rawText, '!HISTORY')) {
                 $fail(__('partcheck.history.invalid'));
             }
-            foreach ($part->history as $hist) {
+            foreach ($part->history ?? [] as $hist) {
                 if (is_null(User::fromAuthor($hist['user'])->first())) {
                     $fail(__('partcheck.history.author'));
                 }
