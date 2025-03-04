40d95904863c356add3ef143c255acf2fc23f60c
Orion Pobursky <orion@ldraw.org>
2025-03-04 20:47:06 +0000

    Add image header imbed to textures

--- app/Console/Commands/DeployUpdate.php
+++ app/Console/Commands/DeployUpdate.php
@@ -3,7 +3,15 @@
 namespace App\Console\Commands;
 
 use App\Enums\PartStatus;
+use App\Enums\PartType;
+use App\Enums\VoteType;
+use App\Events\PartSubmitted;
+use App\LDraw\PartManager;
+use App\LDraw\VoteManager;
 use App\Models\Part\Part;
+use App\Models\Part\PartCategory;
+use App\Models\Part\PartHistory;
+use App\Models\User;
 use App\Settings\LibrarySettings;
 use Illuminate\Console\Command;
 
@@ -28,5 +36,11 @@ class DeployUpdate extends Command
      */
     public function handle(): void
     {
+        Part::whereIn('type', PartType::imageFormat())
+            ->each(function (Part $p) {
+                $body = $p->get();
+                $p->body->body = base64_encode($body);
+                $p->body->save();
+            });
     }
 }
