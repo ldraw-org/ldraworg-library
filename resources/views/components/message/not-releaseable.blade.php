991864eda9e9061edcbdb12d964f2c060ecfb7b8
Orion Pobursky <orion@ldraw.org>
2025-03-21 06:31:11 +0000

    Fix layout

--- resources/views/components/message/not-releaseable.blade.php
+++ resources/views/components/message/not-releaseable.blade.php
@@ -5,25 +5,30 @@
     </x-slot:header>
     <ul>
         @foreach($part->errors as $error => $context)
-            <li wire:key="part-error-{{$loop->iteration}}">
                 @if ($error == 'tracker.uncertsubs')
-                    <x-accordion id="showContents">
-                        <x-slot name="header">
-                            <div>{{__("partcheck.{$error}")}}</div>
-                        </x-slot>
-                        <div class="px-4">
-                            @foreach($part->descendants->whereIn('part_status', [\App\Enums\PartStatus::AwaitingAdminReview, \App\Enums\PartStatus::NeedsMoreVotes, \App\Enums\PartStatus::ErrorsFound]) as $p)
-                                <a wire:key="uncer-subfile-{{$p->id}}" href="{{route('parts.show', $p)}}" class="underline decoration-dotted hover:decoration-solid hover:text-gray-500">{{$p->filename}} ({{$p->part_status->label()}})</a><br/>
-                            @endforeach
-                        </div>
-                    </x-accordion>
+                    <li wire:key="part-error-{{$loop->iteration}}">
+                        <x-accordion id="showContents">
+                            <x-slot name="header">
+                                <div>{{__("partcheck.{$error}")}}</div>
+                            </x-slot>
+                            <div class="px-4">
+                                @foreach($part->descendants->whereIn('part_status', [\App\Enums\PartStatus::AwaitingAdminReview, \App\Enums\PartStatus::NeedsMoreVotes, \App\Enums\PartStatus::ErrorsFound]) as $p)
+                                    <a wire:key="uncer-subfile-{{$p->id}}" href="{{route('parts.show', $p)}}" class="underline decoration-dotted hover:decoration-solid hover:text-gray-500">{{$p->filename}} ({{$p->part_status->label()}})</a><br/>
+                                @endforeach
+                            </div>
+                        </x-accordion>
+                    </li>
                 @else
                     @if ($context)
                         @foreach ($context as $replace)
-                            {{__("partcheck.{$error}", $replace)}}
+                            <li wire:key="part-error-{{$loop->parent->iteration}}-{{$loop->iteration}}">
+                                {{__("partcheck.{$error}", $replace)}}
+                            </li>
                         @endforeach
                     @else
-                        {{__("partcheck.{$error}")}}
+                        <li wire:key="part-error-{{$loop->iteration}}">
+                            {{__("partcheck.{$error}")}}
+                        </li>
                     @endif
                 @endif
             </li>
