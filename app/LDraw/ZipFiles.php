3040a7ee8c7a02cbdb7211bcf0073dad770a2c9d
Orion Pobursky <orion@ldraw.org>
2025-03-23 20:46:09 +0000

    Fix zipfile encoding

--- app/LDraw/ZipFiles.php
+++ app/LDraw/ZipFiles.php
@@ -5,6 +5,7 @@ namespace App\LDraw;
 use Illuminate\Support\Facades\Storage;
 use Spatie\TemporaryDirectory\TemporaryDirectory;
 use App\Models\Part\Part;
+use Illuminate\Database\Eloquent\Collection;
 
 class ZipFiles
 {
@@ -21,22 +22,37 @@ class ZipFiles
             file_put_contents($path, $part->get());
             $time = (int) $part->lastChange()->format('U');
             touch($path, $time);
-            $zip->addFile($path, $part->filename);
+            if ($part->isTexmap()) {
+                $zip->addFile($path, $part->filename, 0, 0, \ZipArchive::FL_ENC_RAW);
+            } else {
+                $zip->addFile($path, $part->filename);
+            }
             $zip->setMtimeName($part->filename, $time);
             $zip->setCompressionName($part->filename, \ZipArchive::CM_DEFAULT);
             $zip->close();
         } else {
             $zip->open(Storage::disk('library')->path('unofficial/ldrawunf.zip'), \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
-            Part::unofficial()->each(function (Part $part) use ($zip, $tempDir) {
-                $path = $tempDir->path($part->filename);
-                file_put_contents($path, $part->get());
-                $time = (int) $part->lastChange()->format('U');
-                touch($path, $time);
-                $zip->addFile($path, $part->filename);
-                $zip->setMtimeName($part->filename, $time);
-                $zip->setCompressionName($part->filename, \ZipArchive::CM_DEFAULT);
-            });
+            $zip->addFile(Storage::disk('library')->path('official/CAreadme.txt'), 'CAreadme.txt');
+            $zip->addFile(Storage::disk('library')->path('official/CAlicense.txt'), 'CAlicense.txt');
+            $zip->addFile(Storage::disk('library')->path('official/CAlicense4.txt'), 'CAlicense4.txt');
             $zip->close();
+            Part::unofficial()->chunk(500, function (Collection $parts) use ($zip, $tempDir) {
+                $zip->open(Storage::disk('library')->path('unofficial/ldrawunf.zip'));
+                $parts->each(function (Part $part) use ($zip, $tempDir) {
+                    $path = $tempDir->path($part->filename);
+                    file_put_contents($path, $part->get());
+                    $time = (int) $part->lastChange()->format('U');
+                    touch($path, $time);
+                    if ($part->isTexmap()) {
+                        $zip->addFile($path, $part->filename, 0, 0, \ZipArchive::FL_ENC_RAW);
+                    } else {
+                        $zip->addFile($path, $part->filename);
+                    }
+                    $zip->setMtimeName($part->filename, $time);
+                    $zip->setCompressionName($part->filename, \ZipArchive::CM_DEFAULT);
+                });
+                $zip->close();
+            });
         }
     }
 }
