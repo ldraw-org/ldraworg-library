2c249508581ee2e02725dcd5fb9681bc199295db
Orion Pobursky <orion@ldraw.org>
2025-03-30 06:23:56 +0000

    Remove errant not operator

--- resources/views/components/release/table.blade.php
+++ resources/views/components/release/table.blade.php
@@ -35,7 +35,7 @@
                 <a class="hover:underline text-blue-600 hover:text-blue-800 visited:text-purple-600" data-pan="update-complete-zip-{{$release->short}}" href="{{asset('library/updates/complete.zip')}}">
                     Complete LDraw.org Library Zip archive (complete.zip)
                 </a>
-                @if(!Storage::disk('library')->exists('updates/LDrawParts.exe'))
+                @if(Storage::disk('library')->exists('updates/LDrawParts.exe'))
                 <a class="hover:underline text-blue-600 hover:text-blue-800 visited:text-purple-600" data-pan="update-complete-exe-{{$release->short}}" href="{{asset('library/updates/LDrawParts.exe')}}">
                     Complete LDraw.org Library Windows installer (LDrawParts.exe)
                 </a>
