bc76b15487487db010ebd13c8b5693d8734068ff
Orion Pobursky <orion@ldraw.org>
2025-04-11 19:15:46 +0000

    Fix icon color

--- resources/views/components/event/icon/index.blade.php
+++ resources/views/components/event/icon/index.blade.php
@@ -4,15 +4,17 @@
     @case(\App\Enums\EventType::Review)
         @empty($event->vote_type)
             <x-library-icon 
-                :icon="\App\Enums\VoteType::CancelVote->icon()" 
-                class="w-8 {{\App\Enums\VoteType::CancelVote->iconColor()}}"
+                :icon="\App\Enums\VoteType::CancelVote->icon()"
+                color="{{\App\Enums\VoteType::CancelVote->iconColor()}}"
+                class="w-8"
                 lowerLeftIcon="{{ !is_null($event->comment) ? 'comment' : null }}"
                 lowerLeftColor="{{\App\Enums\VoteType::Comment->iconColor()}}"
             />
         @else
             <x-library-icon 
                 :icon="$event->vote_type->icon()" 
-                class="w-8 {{$event->vote_type->iconColor()}}" 
+                color="{{$event->vote_type->iconColor()}}"
+                class="w-8" 
                 lowerLeftIcon="{{ !is_null($event->comment) ? 'comment' : null }}"
                 lowerLeftColor="{{\App\Enums\VoteType::Comment->iconColor()}}"
             />
@@ -20,8 +22,9 @@
         @break
     @case(\App\Enums\EventType::Submit)
         <x-library-icon 
-            :icon="$event->event_type->icon()" 
-            class="w-8 {{$event->event_type->iconColor()}}" 
+            :icon="$event->event_type->icon()"
+            color="{{$event->event_type->iconColor()}}"
+            class="w-8" 
             lowerLeftIcon="{{ !is_null($event->comment) ? 'comment' : null }}"
             lowerLeftColor="{{\App\Enums\VoteType::Comment->iconColor()}}"
             lowerRightIcon="{{ $event->initial_submit && !is_null($event->part->official_part) ? 'part-fix' : null }}"
@@ -30,12 +33,13 @@
         @break
     @case(\App\Enums\EventType::Comment)
     @case(\App\Enums\EventType::Release)
-        <x-library-icon :icon="$event->event_type->icon()" class="w-8 {{$event->event_type->iconColor()}}" />
+        <x-library-icon :icon="$event->event_type->icon()" color="{{$event->event_type->iconColor()}}" class="w-8" />
         @break
     @default
         <x-library-icon 
             :icon="$event->event_type->icon()" 
-            class="w-8 {{$event->event_type->iconColor()}}"
+            color="{{$event->event_type->iconColor()}}"
+            class="w-8"
             lowerLeftIcon="{{ !is_null($event->comment) ? 'comment' : null }}"
             lowerLeftColor="{{\App\Enums\VoteType::Comment->iconColor()}}"
         />
