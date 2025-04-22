af05aa24095c48d87252bade49edb3b63d754bef
Orion Pobursky <orion@ldraw.org>
2025-04-22 16:53:59 +0000

    Add menu

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -3,6 +3,9 @@
     <x-slot:breadcrumbs>
         <x-breadcrumb-item class="active" item="User List" />
     </x-slot>
+    <x-slot:menu>
+        <x-menu.library />
+    </x-slot>
     <div class="text-xl font-bold">
         User List
     </div>  
