c60d17c35912bc99de48a670471ffe088c6efb0d
Orion Pobursky <orion@ldraw.org>
2025-04-25 07:05:15 +0000

    Fix quick search position

--- resources/views/livewire/search/menu-item.blade.php
+++ resources/views/livewire/search/menu-item.blade.php
@@ -2,7 +2,7 @@
     <form class="grid grid-col-1 h-full" id="pt_search_comp" action="{{route('parts.list', ['tableSearch' => $search])}}" method="get" name="pt_search_comp">
         <input class="border-inherit border-t border-b-0 border-r-0 md:border-t-0 md:border-l w-full h-full lg:w-fit lg:justify-self-end" name="tableSearch" type="text" wire:model.live="search" wire:input="doSearch" placeholder="Quick Search">
         <div 
-            class="flex flex-col border rounded bg-white absolute mt-2 right-0 w-96 h-72 overflow-scroll z-50 divide-y divide-black"
+            class="flex flex-col border rounded bg-white absolute top-full right-0 w-96 h-72 overflow-scroll z-50 divide-y divide-black"
             x-show="$wire.hasResults"
             x-transition:enter="transition ease-out duration-100" 
             x-transition:enter-start="transform opacity-0"
