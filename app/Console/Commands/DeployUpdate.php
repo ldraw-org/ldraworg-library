a40100f98326414b41ca9acfe7119a0490f074f6
Orion Pobursky <orion@ldraw.org>
2025-09-12 16:52:34 +0000

    Removed old code

--- app/Console/Commands/DeployUpdate.php
+++ app/Console/Commands/DeployUpdate.php
@@ -2,8 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Enums\PartCategory;
-use App\Models\Part\Part;
 use Illuminate\Console\Command;
 
 class DeployUpdate extends Command
@@ -27,18 +25,5 @@ class DeployUpdate extends Command
      */
     public function handle(): void
     {
-        Part::whereNotIn('category', [PartCategory::Sticker, PartCategory::StickerShortcut])
-            ->update(['sticker_sheet_id' => null]);
-        Part::where('category', PartCategory::Moved)
-            ->has('keywords')
-            ->lazy()
-            ->each(function (Part $part) {
-                $part->keywords()->sync([]);
-                $part->generateHeader();
-                if ($part->isOfficial()) {
-                    $part->has_minor_edit = true;
-                    $part->save();
-                }
-            });
     }
 }
