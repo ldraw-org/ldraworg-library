2c010ed31f63f00984654a7c9220a0ce2dcea1aa
Orion Pobursky <orion@ldraw.org>
2025-09-27 06:18:07 +0000

    Remove used code

--- app/Console/Commands/DeployUpdate.php
+++ app/Console/Commands/DeployUpdate.php
@@ -2,12 +2,7 @@
 
 namespace App\Console\Commands;
 
-use App\Jobs\UpdateImage;
-use App\Models\Omr\OmrModel;
-use App\Models\Part\Part;
-use App\Models\Part\PartRelease;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Storage;
 
 class DeployUpdate extends Command
 {
@@ -30,45 +25,6 @@ class DeployUpdate extends Command
      */
     public function handle(): void
     {
-        OmrModel::lazy()
-            ->each(function (OmrModel $model) {
-                $model->clearMediaCollection('image');
-                $model->clearMediaCollection('file');
-                $model->addMediaFromDisk("omr/{$model->filename()}", 'library')
-                    ->preservingOriginal()
-                    ->toMediaCollection('file');
-                if (Storage::disk('images')->exists('omr/models/' . substr($model->filename(), 0, -4) . '.png')) {    
-                    $model->addMediaFromDisk('omr/models/' . substr($model->filename(), 0, -4) . '.png', 'images')
-                        ->preservingOriginal()
-                        ->toMediaCollection('image');
-                } else {
-                    UpdateImage::dispatch($model);
-                }
-            });
-        PartRelease::whereNotNull('part_list')
-            ->each(function (PartRelease $release) {
-                $release->clearMediaCollection('view');
-                foreach($release->part_list as list($description, $filename)) {
-                    $release->addMediaFromDisk('library/updates/view' . $release->short . '/' . substr($filename, 0, -4) . '.png', 'images')
-                        ->preservingOriginal()
-                        ->withCustomProperties([
-                            'description' => $description,
-                            'filename' => $filename,
-                            'id' => Part::official()->where('filename', $filename)->first()?->id,
-                        ])
-                        ->toMediaCollection('view');
-                }
-            });
-        Part::lazy()
-            ->each(function (Part $part) {
-                $part->clearMediaCollection('image');
-                if (Storage::disk('images')->exists("library/{$part->imagePath()}")) {
-                    $part->addMediaFromDisk("library/{$part->imagePath()}", 'images')
-                        ->preservingOriginal()
-                        ->toMediaCollection('image');
-                } else {
-                    UpdateImage::dispatch($part);
-                }
-            });
+        // Nothing yet
     }
 }
--- app/Console/Commands/MassUpdate.php
+++ app/Console/Commands/MassUpdate.php
@@ -33,34 +33,6 @@ class MassUpdate extends Command
      */
     public function handle()
     {
-        $pm = app(PartManager::class);
-
-        // Fix BFC to CCW
-        $vm = app(VoteManager::class);
-        $user = User::find(1);
-
-        Part::with('body')
-            ->official()
-            ->doesntHave('unofficial_part')
-            ->hasError(PartError::BfcNotCcw)
-            ->get()
-            ->each(function (Part $part) use ($pm, $vm, $user) {
-                $upart = $pm->submit(LDrawFile::fromPart($part), $user)->first();
-                $upart->body->body = preg_replace(config('ldraw.patterns.line_type_3'), '3 $1 $8 $9 $10 $5 $6 $7 $2 $3 $4', $upart->body->body);
-                $upart->body->body = preg_replace(config('ldraw.patterns.line_type_4'), '4 $1 $11 $12 $13 $8 $9 $10 $5 $6 $7 $2 $3 $4', $upart->body->body);
-                $upart->body->save();
-
-                $upart->bfc = 'CCW';
-                $upart->history()->create([
-                    'user_id' => 1,
-                    'comment' => 'Changed winding to CCW',
-                ]);
-                $upart->load('history');
-                $upart->generateHeader();
-                $upart->save();
-                $pm->checkPart($upart);
-                $vm->castVote($upart, $user, VoteType::AdminFastTrack);
-            });
-
+        // Nothing yet
     }
 }
