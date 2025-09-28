ddf1133abba454bc9dfbf780a33b40e9787002b0
Orion Pobursky <orion@ldraw.org>
2025-09-28 17:16:25 +0000

    Switch to job coll for library CSV.

--- routes/console.php
+++ routes/console.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Jobs\UpdateLibraryCsv;
 use Illuminate\Support\Facades\Schedule;
 use App\LDraw\ScheduledTasks\SendDailyDigest;
 use App\LDraw\ScheduledTasks\UpdateTrackerHistory;
 
 Schedule::command('queue:prune-batches')->daily();
-Schedule::command('lib:refresh-csv')->everyFiveMinutes();
+Schedule::job(new UpdateLibraryCsv(), 'maintenance')->everyFiveMinutes();
 Schedule::call(new SendDailyDigest())->dailyAt('01:30')->environments(['production']);
 Schedule::call(new UpdateTrackerHistory())->daily();
 Schedule::command('lib:daily-maintenance')->daily();
