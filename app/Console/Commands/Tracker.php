fa045f75c9470cec61902a82819a78d8dfde1c06
Orion Pobursky <orion@ldraw.org>
2025-10-01 17:24:13 +0000

    Tracker command

---
+++ app/Console/Commands/Tracker.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Settings\LibrarySettings;
+use Illuminate\Console\Command;
+
+class Tracker extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'lib:tracker {--lock} {--unlock}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'General Tracker operations';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(LibrarySettings $settings)
+    {
+        if ($this->option('lock')) {
+            $settings->tracker_locked = true;
+            $this->info('Tracker is locked');
+        } elseif ($this->option('unlock')) {
+            $settings->tracker_locked = false;
+            $this->info('Tracker is unlocked');
+        }
+        
+        $settings->save();
+    }
+}
