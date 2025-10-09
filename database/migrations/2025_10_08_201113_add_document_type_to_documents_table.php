16d94cddb731e680063146b9015b5e56f180af1a
Orion Pobursky <orion@ldraw.org>
2025-10-09 00:13:46 +0000

    commit missed migration

---
+++ database/migrations/2025_10_08_201113_add_document_type_to_documents_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Enums\DocumentType;
+use App\Models\Document\Document;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('documents', function (Blueprint $table) {
+            $table->string('type')->default('md')->index();
+        });
+        Document::query()->update(['type' => DocumentType::Markdown]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('documents', function (Blueprint $table) {
+            $table->dropColumn('type');
+        });
+    }
+};
