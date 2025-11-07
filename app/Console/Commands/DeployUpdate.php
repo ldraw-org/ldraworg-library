4f45fe52ed431a06ecbded550481312e1a1470d2
Orion Pobursky <orion@ldraw.org>
2025-11-07 07:43:55 +0000

    Remove used code

--- app/Console/Commands/DeployUpdate.php
+++ app/Console/Commands/DeployUpdate.php
@@ -2,10 +2,7 @@
 
 namespace App\Console\Commands;
 
-use App\Enums\PartStatus;
-use App\Models\TrackerHistory;
 use Illuminate\Console\Command;
-use Illuminate\Support\Arr;
 
 class DeployUpdate extends Command
 {
@@ -28,16 +25,6 @@ class DeployUpdate extends Command
      */
     public function handle(): void
     {
-        TrackerHistory::each(function (TrackerHistory $trackerHistory) {
-            $data = $trackerHistory->history_data;
-            if (Arr::has($data, 5)) {
-                $data[6] = $data[5];
-            }
-            if (Arr::has($data, 4)) {
-                $data[5] = $data[4];
-            }
-            $trackerHistory->history_data = $data;
-            $trackerHistory->save();
-        });
+
     }
 }
