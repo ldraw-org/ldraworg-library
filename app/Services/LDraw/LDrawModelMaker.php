d5a7438a0058bec9abdb2509c4ccdf9a423ba8c8
Orion Pobursky <orion@ldraw.org>
2025-11-14 07:20:40 +0000

    Streamline model creation

--- app/Services/LDraw/LDrawModelMaker.php
+++ app/Services/LDraw/LDrawModelMaker.php
@@ -7,6 +7,7 @@ use App\Models\Part\Part;
 use App\Services\Parser\ImprovedParser;
 use App\Services\Parser\ParsedPartCollection;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection as SupportCollection;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -17,25 +18,41 @@ class LDrawModelMaker
         if ($part->isTexmap()) {
             return $part->get(true, true);
         }
+        $file = [];
         $topModelName = basename($part->filename, '.dat') . '.ldr';
         $preview = $part->preview ?? '16 0 0 0 1 0 0 0 1 0 0 0 1';
-        $file = "0 FILE {$topModelName}\r\n1 {$preview} {$part->meta_name}\r\n0 FILE {$part->meta_name}\r\n{$part->get()}\r\n";
-        if ($part->isUnofficial()) {
-            $sparts = $part->descendants()->doesntHave('unofficial_part')->get()->unique('filename');
-        } else {
-            $sparts = $part->descendants->official()->unique('filename');
-        }
-        $sparts->load('body');
-        $sparts->each(function (Part $p) use (&$file) {
+        $file[] = "0 FILE {$topModelName}";
+        $file[] = "1 {$preview} {$part->meta_name}";
+        $file[] = "0 FILE {$part->meta_name}";
+        $file[] = $part->get();
+        $this->getPartSubparts($part)
+          ->each(function (Part $p) use (&$file) {
             if ($p->isTexmap()) {
-                $file .= $p->get(true, true);
+                $file[] = $p->get(true, true);
             } else {
-                $file .= "0 FILE {$p->meta_name}\r\n{$p->get()}\r\n";
+                $file[] = "0 FILE {$p->meta_name}";
+                $file[] = $p->get();
             }
         });
-        return $file;
+        return implode("\r\n", $file);
     }
 
+    protected function getPartSubparts(Part $part, $includeSelf = false): Collection
+    {
+        $query = $includeSelf ? $part->descendantsAndSelf() : $part->descendants();
+
+        $query = $part->isUnofficial()
+            ? $query->doesntHave('unofficial_part')
+            : $query->official();
+
+        $subpartFilenames = $query->distinct()->pluck('filename');
+        
+        return Part::with('body')
+            ->select('id', 'filename', 'type', 'header')
+            ->whereIn('filename', $subpartFilenames)
+            ->get();
+    }
+  
     public function modelMpd(string|OmrModel $model): string
     {
         if ($model instanceof OmrModel) {
@@ -43,88 +60,100 @@ class LDrawModelMaker
         } else {
             $file = $model;
         }
-        $subparts = collect((new ParsedPartCollection($file))->subparts($file));
-        $subs = $subparts
-            ->map( function (string $subpart) {
-                $subpart = Str::of($subpart)->replace('\\', '/');
-                if (pathinfo($subpart, PATHINFO_EXTENSION) == 'png') {
-                    return $subpart->prepend('parts/textures/')->toString();
-                }
-                return $subpart->prepend('parts/')->toString();
-            })
-            ->merge(
-                $subparts
-                ->map( function (string $subpart) {
-                    $subpart = Str::of($subpart)->replace('\\', '/');
-                    if (pathinfo($subpart, PATHINFO_EXTENSION) == 'png') {
-                        return $subpart->prepend('p/textures/')->toString();
-                    }
-                    return $subpart->prepend('p/')->toString();
-                })
-            )
-            ->all();
-        $parts = new Collection();
-        Part::with('descendantsAndSelf')
-            ->doesntHave('unofficial_part')
-            ->whereIn('filename', $subs)
-            ->each(function (Part $p) use (&$parts) {
-                $parts = $parts->merge($p->descendantsAndSelf()->doesntHave('unofficial_part')->get()->unique('filename')->all())->unique('filename');
-            });
-        $parts
-            ->each(function (Part $p) use (&$file) {
+        $subpartFilenames = (new ParsedPartCollection($file))->subpartFilenames($file) ?? [];
+        $allLibraryFilenames = [];
+        Part::whereIn('filename', $subpartFilenames)
+            ->each(function (Part $part) use (&$allLibraryFilenames) {
+                $this->getPartSubparts($part, true)
+                    ->each(function (Part $p) use (&$allLibraryFilenames) {
+                        $allLibraryFilenames[$p->filename] = true;
+                    });
+            }); 
+        $lines = [];
+        Part::with('body')
+          ->select('id', 'filename', 'type', 'header')
+          ->whereIn('filename', array_keys($allLibraryFilenames))
+          ->each(function (Part $p) use (&$lines) {
                 if ($p->isTexmap()) {
-                    $file .= $p->get(true, true);
+                    $lines[] = $p->get(true, true);
                 } else {
-                    $file .= "0 FILE {$p->meta_name}\r\n{$p->get()}\r\n";
+                    $lines[] = "0 FILE {$p->meta_name}";
+                    $lines[] = $p->get();
                 }
             });
-        return $file;
+        return $file .= implode("\r\n", $lines) . "\r\n";;
     }
+
 
     public function diff(Part $part1, Part $part2): string
     {
-        $lines = collect(explode("\n", $part1->body->body))->filter(function (string $value) {
-            return !empty($value) && $value[0] != "0";
-        });
-        $lines2 = collect(explode("\n", $part2->body->body))->filter(function (string $value) {
-            return !empty($value) && $value[0] != "0";
-        });
-        $pattern = '#^([12345]) (\d+)#';
-        $delcolor   = ['1' => '36', '2' => '12', '3' => '36', '4' => '36', '5' => '12'];
-        $addcolor   = ['1' =>  '2', '2' => '10', '3' =>  '2', '4' =>  '2', '5' => '10'];
-        $matchcolor = ['1' => '15', '2' =>  '8', '3' => '15', '4' => '15', '5' =>  '8'];
-        $same = $lines->intersect($lines2)->transform(function (string $item) use ($pattern, $matchcolor) {
-            return preg_replace($pattern, '$1 '. $matchcolor[$item[0]], $item);
-        });
-        $added = $lines2->diff($lines)->transform(function (string $item) use ($pattern, $addcolor) {
-            return preg_replace($pattern, '$1 '. $addcolor[$item[0]], $item);
-        });
-        $removed = $lines->diff($lines2)->transform(function (string $item) use ($pattern, $delcolor) {
-            return preg_replace($pattern, '$1 '. $delcolor[$item[0]], $item);
-        });
-        return implode("\n", array_merge($same->toArray(), $added->toArray(), $removed->toArray()));
+        $lines1 = collect(explode("\n", $part1->body->body))
+            ->filter(fn($line) => $line !== '' && $line[0] !== '0')
+            ->values();
+    
+        $lines2 = collect(explode("\n", $part2->body->body))
+            ->filter(fn($line) => $line !== '' && $line[0] !== '0')
+            ->values();
+    
+        $delcolor   = ['1'=>'36','2'=>'12','3'=>'36','4'=>'36','5'=>'12'];
+        $addcolor   = ['1'=>'2','2'=>'10','3'=>'2','4'=>'2','5'=>'10'];
+        $matchcolor = ['1'=>'15','2'=>'8','3'=>'15','4'=>'15','5'=>'8'];
+    
+        $m = $lines1->count();
+        $n = $lines2->count();
+    
+        $prev = array_fill(0, $n + 1, 0);
+        $curr = array_fill(0, $n + 1, 0);
+    
+        for ($i = 0; $i < $m; $i++) {
+            for ($j = 0; $j < $n; $j++) {
+                $curr[$j+1] = ($lines1[$i] === $lines2[$j])
+                    ? $prev[$j] + 1
+                    : max($prev[$j+1], $curr[$j]);
+            }
+            [$prev, $curr] = [$curr, $prev];
+        }
+    
+        $diffLines = collect();
+        $backtrack = function($i, $j) use (&$lines1, &$lines2, &$delcolor, &$addcolor, &$matchcolor, &$prev, &$diffLines, &$backtrack) {
+            if ($i > 0 && $j > 0 && $lines1[$i-1] === $lines2[$j-1]) {
+                $backtrack($i-1, $j-1);
+                $diffLines->push(
+                    preg_replace_callback('#^([12345]) (\d+)#', fn($m) => $m[1] . ' ' . $matchcolor[$m[1]], $lines1[$i-1])
+                );
+            } elseif ($j > 0 && ($i === 0 || $prev[$j] >= $prev[$j-1])) {
+                $backtrack($i, $j-1);
+                $diffLines->push(
+                    preg_replace_callback('#^([12345]) (\d+)#', fn($m) => $m[1] . ' ' . $addcolor[$m[1]], $lines2[$j-1])
+                );
+            } elseif ($i > 0) {
+                $backtrack($i-1, $j);
+                $diffLines->push(
+                    preg_replace_callback('#^([12345]) (\d+)#', fn($m) => $m[1] . ' ' . $delcolor[$m[1]], $lines1[$i-1])
+                );
+            }
+        };
+    
+        $backtrack($m, $n);
+    
+        return $diffLines->implode("\n");
     }
 
     public function webGl(string|OmrModel|Part $model): array
     {
         $webgl = [];
         if ($model instanceof Part) {
-            if ($model->isUnofficial()) {
-                $sparts = $model->descendantsAndSelf()->doesntHave('unofficial_part')->get()->unique('filename');
-            } else {
-                $sparts = $model->descendantsAndSelf->official()->unique('filename');
-            }
-            $sparts->load('body');
-            $sparts->each(function (Part $p) use (&$webgl) {
-                $text = base64_encode($p->get());
-                $name = Str::chopStart($p->filename, ['parts/', 'p/']);
-                if ($p->isTexmap()) {
-                    $name = Str::chopStart($name, 'textures/');
-                    $webgl[$name] = "data:img/png;base64,{$text}";
-                } else {
-                    $webgl[$name] = "data:text/plain;base64,{$text}";
-                }
-            });
+            $this->getPartSubparts($model, true)
+                ->each(function (Part $p) use (&$webgl) {
+                    $text = base64_encode($p->get());
+                    $name = Str::chopStart($p->filename, ['parts/', 'p/']);
+                    if ($p->isTexmap()) {
+                        $name = Str::chopStart($name, 'textures/');
+                        $webgl[$name] = "data:img/png;base64,{$text}";
+                    } else {
+                        $webgl[$name] = "data:text/plain;base64,{$text}";
+                    }
+                });
         } elseif ($model instanceof OmrModel) {
             $webgl[$model->filename()] = 'data:text/plain;base64,' . base64_encode($this->modelMpd($model));
         } else {
