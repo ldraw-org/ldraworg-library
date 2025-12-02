33621718da2147779d4744e118088ee3e821a48b
Orion Pobursky <orion@ldraw.org>
2025-12-02 02:32:23 +0000

    Fix files with improper keywords

--- app/Console/Commands/DeployUpdate.php
+++ app/Console/Commands/DeployUpdate.php
@@ -2,12 +2,10 @@
 
 namespace App\Console\Commands;
 
-use App\Enums\PartCategory;
-use App\Jobs\UpdateRebrickable;
 use App\Models\Part\Part;
-use App\Models\RebrickablePart;
-use App\Services\LDraw\Managers\Part\PartManager;
+use App\Models\Part\PartKeyword;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
 
 class DeployUpdate extends Command
 {
@@ -28,12 +26,52 @@ class DeployUpdate extends Command
     /**
      * Execute the console command.
      */
-    public function handle(PartManager $partManager): void
+    public function handle(): void
     {
+        $partIds = collect();
+        PartKeyword::whereLike('keyword', '{"id":%')
+            ->each(function (PartKeyword $keyword) use (&$partIds) {
+                preg_match('~"keyword":"(.*?)"~', $keyword->keyword, $match);
+                $newKeyword = PartKeyword::updateOrCreate(['keyword' => $match[1]]);
+                $oldId = $keyword->id;
+                $newId = $newKeyword->id;
+                $partIds = $partIds->merge($keyword->parts->pluck('id'));
+                $keyword->parts->each(function (Part $part) use ($oldId, $newId) {
+                    $ids = $part->keywords
+                        ->pluck('id')
+                        ->filter(fn ($id) => $id != $oldId)
+                        ->push($newId)
+                        ->unique()
+                        ->values()
+                        ->all();
+                    $part->keywords()->sync($ids);
+                });
+            });
+        $ids = $partIds->flatten()->unique()->values()->all();
+        $updateZipPath = Storage::disk('library')->path('updates/lcad2511.zip');
+        $completeZipPath = Storage::disk('library')->path('updates/complete.zip');
+        $updateZip = new \ZipArchive();
+        $completeZip = new \ZipArchive();
+        Part::whereIn('id', $ids)
+            ->each(function (Part $part) use ($updateZipPath, $completeZipPath, $updateZip, $completeZip) {
+                $part->has_minor_edit = true;
+                $part->generateHeader();
+                app(\App\Services\LDraw\Managers\Part\PartManager::class)->checkPart($part);
+                $updateZip->open($updateZipPath);
+                $completeZip->open($completeZipPath);
+                $filename = "ldraw/{$part->filename}";
+                $file = $part->get();
+                $updateZip->deleteName($filename);
+                $completeZip->deleteName($filename);
+                $timestamp = $part->lastChange()->getTimestamp();
+                
+                $updateZip->addFromString($filename, $file);
+                $updateZip->setMtimeName($filename, $timestamp);
+                $completeZip->addFromString($filename, $file);
+                $completeZip->setMtimeName($filename, $timestamp);
 
-        Part::partsFolderOnly()
-            ->each(function (Part $part) use ($partManager) {
-                $partManager->updateBasePart($part);
+                $updateZip->close();
+                $completeZip->close();
             });
     }
 }
