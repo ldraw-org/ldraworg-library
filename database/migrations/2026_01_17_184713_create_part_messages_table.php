64abb5bf55a9b8143b32a0372d9deac9478732d4
Orion Pobursky <orion@ldraw.org>
2026-01-23 16:18:09 -0800

    fix migration column part_id

--- database/migrations/2026_01_17_184713_create_part_messages_table.php
+++ database/migrations/2026_01_17_184713_create_part_messages_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('part_messages', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->ForeignFor(Part::class);
+            $table->foreignIdFor(Part::class);
             $table->string('check');
             $table->string('check_type');
             $table->integer('line_number');
