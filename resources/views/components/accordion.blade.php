6475a619a7f022cb7dc6f564673b4bd04d542764
Orion Pobursky <orion@ldraw.org>
2026-03-21 20:19:49 -0700

    Improve accordion and add default open

--- resources/views/components/accordion.blade.php
+++ resources/views/components/accordion.blade.php
@@ -1,16 +1,33 @@
-@props(['header'])
+@props(['header', 'open' => false])
 
-<div x-data="{ {{$attributes->get('id')}} : false }" class="flex flex-col">
-    <div {{$header->attributes->class(['flex flex-row'])}} >
-        <x-library-icon icon="menu-right" class="w-7 place-self-center cursor-pointer" ::class="{{$attributes->get('id')}} && 'rotate-90'" @@click="{{$attributes->get('id')}} = !{{$attributes->get('id')}}" />
-        <div class="select-none cursor-pointer" @@click="{{$attributes->get('id')}} = !{{$attributes->get('id')}}">{{$header}}</div>
+<div x-data="{ open: @js($open) }" class="flex flex-col">
+
+    <div {{ $header->attributes->class(['flex flex-row items-center gap-1']) }}>
+
+        <x-library-icon
+            icon="menu-right"
+            class="w-7 cursor-pointer transition-transform"
+            ::class="{ 'rotate-90': open }"
+            @click="open = !open"
+        />
+
+        <div
+            class="select-none cursor-pointer"
+            @click="open = !open"
+        >
+            {{ $header }}
+        </div>
+
     </div>
-    <div 
-        x-show="{{$attributes->get('id')}}" 
-        x-transition:enter="transition ease-out duration-100" 
-        x-transition:enter-start="transform opacity-0"
+
+    <div
+        x-show="open"
+        x-transition:enter="transition ease-out duration-150"
+        x-transition:enter-start="opacity-0 -translate-y-1"
+        x-transition:enter-end="opacity-100 translate-y-0"
         x-cloak
     >
-        {{$slot}}
+        {{ $slot }}
     </div>
+
 </div>
