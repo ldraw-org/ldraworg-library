dd303a2c9b8ca56c771a39f99c248f690f889cb7
Orion Pobursky <orion@ldraw.org>
2026-05-06 17:45:22 -0700

    Ensure new part bodies have image created

--- app/Observers/PartBodyObserver.php
+++ app/Observers/PartBodyObserver.php
@@ -19,13 +19,10 @@ class PartBodyObserver
     ) {}
     public function saved(PartBody $body): void
     {
-        if ($body->wasChanged()) {
+        if ($body->wasRecentlyCreated || $body->wasChanged('body')) {
             $this->subpartSync->loadSubparts($body->part);
             $this->imageGenerator->regenerateImage($body->part);
             $body->part->ancestors->each(fn (Part $part) => GeneratePartImage::dispatch($part->id));
-        }
-        if (config('ldraw.library_debug')) {
-            Log::debug("Saved part body {$body->part_id} ({$body->part->filename})");
         }
     }
 }
