78bad99a00142a17c62ad40252d437df18177fa2
Orion Pobursky <orion@ldraw.org>
2026-05-31 23:12:30 -0700

    streamline library.csv

--- app/Services/LDraw/SupportFiles.php
+++ app/Services/LDraw/SupportFiles.php
@@ -6,6 +6,7 @@ use App\Enums\PartCategory;
 use App\Models\Part\Part;
 use App\Models\Part\PartRelease;
 use Illuminate\Support\Facades\Storage;
+use League\Csv\Writer;
 
 class SupportFiles
 {
@@ -16,29 +17,40 @@ class SupportFiles
 
     public static function setLibraryCsv(): void
     {
-        $csv = "part_number,part_description,part_url,image_url,image_last_modified\n";
-        $csv .= Part::select('id', 'filename', 'description', 'part_release_id')
+        $headers = ['part_number', 'part_description', 'part_url', 'image_url', 'image_last_modified', 'category'];
+
+        $rows = Part::select('id', 'filename', 'description', 'part_release_id', 'created_at', 'category')
             ->with('media')
             ->doesntHave('official_part')
             ->partsFolderOnly()
-            ->whereNotLike('description', '=%')
+            ->activeParts()
+            ->whereNull('type_qualifier')
             ->whereNotLike('description', '|%')
             ->whereNotLike('description', '~%')
-            ->whereNotLike('description', '\_%')
-            ->whereNotIn('category', [PartCategory::Moved, PartCategory::Obsolete])
             ->get()
             ->map(function (Part $part) {
                 $media = $part->getFirstMedia('image');
-                return basename($part->filename)
-                    . ',"' . str_replace('"', '""', $part->description) . '"'
-                    . ',' . route('part.download', ['library' => is_null($part->part_release_id) ? 'unofficial' : 'official', 'filename' => $part->filename])
-                    . ',' . version('media/ldraw/' . $part->libFolder() . '/' . substr($part->filename, 0, -3) . 'png')
-                    . ',' . $media?->created_at->format('Y-m-d') ?? $part->created_at->format('Y-m-d');
-            })
-            ->implode("\n");
-        Storage::put('library/library.csv', $csv);
-    }
+                $imageDate = ($media?->created_at ?? $part->created_at)->format('Y-m-d');
+                if ($part->category === null) dd($part->filename);
+                return [
+                    basename($part->filename),
+                    $part->description,
+                    route('part.download', [
+                        'library'   => is_null($part->part_release_id) ? 'unofficial' : 'official',
+                        'filename'  => $part->filename,
+                    ]),
+                    $part->getFirstMediaUrl('image'),
+                    $imageDate,
+                    $part->category->value,
+                ];
+            });
 
+        $csv = Writer::fromString();
+        $csv->setEscape('');
+        $csv->insertOne($headers);
+        $csv->insertAll($rows);
+        Storage::put('library/library.csv', $csv->toString());
+    }
     public static function ptReleases(string $output = "xml"): string
     {
         $releases = PartRelease::where('short', '!=', 'original')->oldest()->get();
