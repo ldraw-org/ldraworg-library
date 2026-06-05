51dc217b3356efcefce1c2dfb5aba1348a8ee414
Orion Pobursky <orion@ldraw.org>
2026-06-05 11:21:35 -0700

    Update LDView build and debug

--- app/Services/Render/LDView.php
+++ app/Services/Render/LDView.php
@@ -53,12 +53,11 @@ class LDView
         $options = [
             'ProcessLDConfig' => '1',
             'LDConfig' => $ldconfigPath,
-            'LDrawDir' => $ldrawdir,
+            'VerifyLDrawDir' => '0',
             'SaveWidth' => $width * 2,
             'SaveHeight' => $height * 2,
             'SaveAlpha' => '1',
             'SaveZoomToFit' => '1',
-            'SaveSnapshot' => $imagepath,
             'BFC' => '0',
             'FOV' => '0.1',
             'Texmaps' => '1',
@@ -81,10 +80,12 @@ class LDView
 
         $cmdOptions = collect($options)
             ->map(fn (string $value, string $command) => "-{$command}={$value}")
-            ->implode(' ');
+            ->implode("\n");
+        $iniPath = $tempDir->path("ldview.ini");
+        file_put_contents($iniPath, "[General]\n{$cmdOptions}\n");
 
         // Run LDView
-        $ldviewcmd = "ldview {$filename} {$cmdOptions}";
+        $ldviewcmd = "ldview {$filename} -IniFile={$iniPath} -LDrawDir={$ldrawdir} -SaveSnapshot={$imagepath}";
         if ($this->debug) {
             Log::debug($ldviewcmd);
         }
@@ -93,13 +94,20 @@ class LDView
         if ($this->debug) {
             Log::debug($result->output());
             Log::debug($result->errorOutput());
-            Storage::put("/debug/part.mpd", file_get_contents($filename));
+            Storage::put("debug/part.mpd", file_get_contents($filename));
+            Storage::put("/debug/ldview.ini", file_get_contents($iniPath));
+            Log::debug("ldview " . Storage::path('debug/part.mpd') . " -IniFile=" . Storage::path('debug/ldview.ini'). " -LDrawDir=" . Storage::path('debug/ldraw') . " -SaveSnapshot=" . Storage::path('debug/part.png'));
+            if (file_exists($imagepath)) {
+                Storage::put("/debug/part.png", file_get_contents($imagepath));
+            } else {
+                Log::debug('No image found');
+            }
         }
 
         if (!file_exists($imagepath)) {
             file_put_contents($imagepath, base64_decode("iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII="));
         } else {
-            $image = Image::load($imagepath)
+            Image::load($imagepath)
                 ->fit(Fit::Contain, $width, $height)
                 ->optimize($this->optimizer)
                 ->save();
