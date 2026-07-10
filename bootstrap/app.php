3f9f63339ca2213350a8fbf49eedfb2b67be21eb
Orion Pobursky <orion@ldraw.org>
2026-07-09 20:02:34 -0700

    Remove mybb cookie exception and add trusted proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,9 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'currentlic' => App\Http\Middleware\CurrentLicense::class,
             'ldrawmember' => App\Http\Middleware\LdrawMember::class
         ]);
-        $middleware->encryptCookies(except: [
-            'mybbuser',
-        ]);
+        $middleware->trustProxies(at: '127.0.0.1');
         $middleware->redirectGuestsTo('/login');
     })
     ->withExceptions(function (Exceptions $exceptions) {
